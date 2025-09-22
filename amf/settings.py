94106f8646738e9f55a266a60f84cbdd49036171
shameer <shameershajahan17@gmail.com>
2025-09-22 17:18:15 +0530

    fix the settings

--- amf/settings.py
+++ amf/settings.py
@@ -83,6 +83,18 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'admin',
+        'PASSWORD': 'Hostlane#92!',   
+        'HOST': 'amf.cts6c80s8w14.eu-north-1.rds.amazonaws.com',
+        'PORT': '3306',
+    }
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
