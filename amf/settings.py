2256ca4682df00480f552b10c5308cae5d1adeb9
shameer shajahan <shameershajahan17@gmail.com>
2025-09-24 14:32:29 +0530

    Update settings.py
    db in settings

--- amf/settings.py
+++ amf/settings.py
@@ -74,14 +74,6 @@ WSGI_APPLICATION = 'amf.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 DATABASES = {
     'default': {
